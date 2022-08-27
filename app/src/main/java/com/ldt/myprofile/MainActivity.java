ac29435248f7cf46eb1b12a1648ded38d9c291fc
LD-Tien <leductien00@gmail.com>
2022-08-27 23:06:04 +0700

    Upload source code

---
+++ app/src/main/java/com/ldt/myprofile/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ldt.myprofile;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMyProfile();
+            }
+        });
+    }
+    public void openMyProfile() {
+        Intent intent = new Intent(this, MyProfile.class);
+        startActivity(intent);
+    }
+}
